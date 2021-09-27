6f80f6d429824e673b91afa20c33fbf18fc73ee5
tnaake <thomasnaake@gmx.de>
2021-09-27 10:37:50 +0200

    add tests for initialize_server, check for correct inputs of missingValue and envir arguments

--- tests/testthat/test_shinyQC.R
+++ tests/testthat/test_shinyQC.R
@@ -16,13 +16,21 @@ test_that("shinyQC", {
     colnames(se) <- 1:8
     tmp <- 1
     expect_error(shinyQC(tmp), "se is not of class 'SummarizedExperiment'")
-    se_tmp <- se
-    colnames(colData(se_tmp))[1] <- "rowname"
-    expect_error(shinyQC(se_tmp), 
-        "colData[(]se[)] should not have column 'rowname'")
 })
 
 ## function .initialize_server
 test_that(".initialize_server", {
+    se <- SummarizedExperiment::SummarizedExperiment(
+        assays = list(counts = matrix(100 * runif(100 * 8), 100, 8)),
+        colData = S4Vectors::DataFrame(sample = paste0("S", 1:8),
+                                       group = sample(LETTERS[1:2], 8, replace = TRUE),
+                                       name = paste0("S", 1:8))
+    )
+    expect_error(MatrixQCvis:::.initialize_server(se = se, input = new.env(),
+        output = new.env, session = new.env(), missingValue = NULL,
+        envir = new.env()), "missingValue has to be logical")
+    expect_error(MatrixQCvis:::.initialize_server(se = se, input = new.env(),
+        output = new.env, session = new.env(), missingValue = TRUE,
+        envir = NULL), "envir has to be of class environment")
     expect_is(MatrixQCvis:::.initialize_server, "function")
 })
